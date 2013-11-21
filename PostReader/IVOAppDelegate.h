d33c1a5c8364c6f444023f394674be110ab20055
invivo <will.kwok@invivointeractive.com>
2013-11-21 12:11:15 +0800

    another init commit

---
+++ PostReader/IVOAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  IVOAppDelegate.h
+//  PostReader
+//
+//  Created by Yu Ho Kwok on 21/11/13.
+//  Copyright (c) 2013 Yu Ho Kwok. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface IVOAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ PostReader/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  PostReader
+//
+//  Created by Yu Ho Kwok on 21/11/13.
+//  Copyright (c) 2013 Yu Ho Kwok. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "IVOAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([IVOAppDelegate class]));
+    }
+}
